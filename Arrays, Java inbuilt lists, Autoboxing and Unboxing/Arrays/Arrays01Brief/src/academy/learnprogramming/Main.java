6723d152617664632434d501be4c3b443526456e
AntonioBaciu <antoniopaulobaciu@gmail.com>
2021-07-30 11:02:23 +0200

    Uploading Arrays examples, now I'll have to take it apart and understand it

---
+++ Arrays, Java inbuilt lists, Autoboxing and Unboxing/Arrays/Arrays01Brief/src/academy/learnprogramming/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // #1st way
+
+        int[] myIntArray = new int[25]; // #2nd way
+
+//        myIntArray[0] = 45;
+//        myIntArray[1] = 476;
+//        myIntArray[5] = 50;
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+        printArray(myIntArray);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
+        }
+    }
+}
